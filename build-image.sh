d3f90b80174d65cc2f15526faaaa7d75dd5a8935
bitbytejoy <bitbytejoy@gmail.com>
2023-01-09 15:04:31 +0100

    init: BIG BANG

---
+++ build-image.sh
@@ -0,0 +1,2 @@
+mvn clean package
+docker build -t spring-boot-docker-demo:latest .
---
+++ src/main/java/de/neuefische/neuefischespringbootdockerdemo/NeuefischeSpringBootDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.neuefischespringbootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeSpringBootDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuefischeSpringBootDockerDemoApplication.class, args);
+    }
+
+}
